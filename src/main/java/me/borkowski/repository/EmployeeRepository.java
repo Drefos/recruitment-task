48c616834ab9cffbbde638abf26dbebc6de4607b
Maciej Borkowski <maciekbb98@gmail.com>
2018-04-23 15:28:11 +0200

    Added method to EmployeeRepository which returns list of PositionStatistics

--- src/main/java/me/borkowski/repository/EmployeeRepository.java
+++ src/main/java/me/borkowski/repository/EmployeeRepository.java
@@ -1,7 +1,14 @@
 package me.borkowski.repository;
 
 import me.borkowski.model.Employee;
+import me.borkowski.model.PositionStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT new me.borkowski.model.PositionStatistics(e.position, count(e)) FROM Employee e GROUP BY e.position")
+    List<PositionStatistics> countEmployeesPositions();
 }
